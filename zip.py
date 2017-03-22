bfba6a3ad3d5a6d41b1c32e52e52f8cfdb3e5420
Eric Miller <eric@legoaces.org>
2017-03-22 00:57:56 -0400

    Renamed a bunch of images

--- zip.py
+++ zip.py
@@ -1,61 +1,21 @@
 #!/usr/bin/env python
-
 import os
 
-DO_MANUAL_CHECK = False
+import itertools
+import shutil
 
-def get_possible_names():
-    file = open('Olin Student Directory.txt')
-    lines = file.readlines()
-    lines = [l.strip() for l in lines]
+names = open('filtered_names.txt').readlines()
+names = [n.strip() for n in names]
 
-    def isLikelyName(line):
-        if '/' in line:
-            return False
-        if ',' in line:
-            return False
-        if len(line) <= 5:
-            return False
+images = os.listdir('images')
+images = [i for i in images if i[0] != '.']
+images.sort()
 
-        badPhrases = ['olin', 'college', 'student', 'directory', 'class of']
+assert len(names) == len(images)
 
-        for word in badPhrases:
-            if line.lower().count(word):
-                return False
-        return True
+print zip(names, images)
 
-    return filter(isLikelyName, lines)
+os.makedirs('renamed_images')
+for name, image in zip(names, images):
+    shutil.copyfile('images/{}'.format(image), 'renamed_images/{}.jpg'.format(name))
 
-
-def filter_names_by_user(names):
-    newNames = []
-
-    print "When presented with each name, type 'n' before pressing enter\n \
-          if the name is invalid"
-    for name in names:
-        command = raw_input('"{}"? [y]/n : '.format(name))
-        if command.lower() != 'n':
-            newNames.append(name)
-
-    return newNames
-
-
-def save_names(names, file='filtered_names.txt'):
-    f = open(file, 'w')
-    f.writelines([n + '\n' for n in names])
-    f.close()
-
-
-def sanitize_names(names):
-    names = [n.title() for n in names]
-    return names
-
-
-if __name__ == '__main__':
-    names = get_possible_names()
-    names = sanitize_names(names)
-    if DO_MANUAL_CHECK:
-        names = filter(names)
-
-    save_names(names)
-    print names, len(names)
